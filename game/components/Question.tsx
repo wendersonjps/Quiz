ce593ca637b638beda2e7db72c45b7a8b54386ea
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-02 18:58:24 -0300

    :hammer: Integrating Answer component and created a new function

--- game/components/Question.tsx
+++ game/components/Question.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Question.module.css'
 import QuestionModel from '../model/question'
 import Wording from './Wording'
+import Answer from './Answer'
 
 interface QuestionProps {
     value: QuestionModel
@@ -9,9 +10,18 @@ interface QuestionProps {
 export default function Question(props: QuestionProps) {
     const question = props.value
 
+    function renderAnswer() {
+        return question.answer.map((answer, i) => {
+            return (
+                <Answer value={answer} indice={i} letter='A' letterColor='#F2C866' />
+            )
+        })
+    }
+
     return (
         <div className={styles.question}>
             <Wording text={question.wording} />
+            {renderAnswer()}
         </div>
     )
 }
