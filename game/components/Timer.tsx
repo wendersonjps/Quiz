8fc3485da53dab3706df0c4851e66019ab2af15d
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-03 19:55:32 -0300

    :hammer: Added a new attribute into the CountedownCircleTimer component

--- game/components/Timer.tsx
+++ game/components/Timer.tsx
@@ -10,8 +10,8 @@ interface TimerProps {
 export default function Timer(props: TimerProps) {
     return (
         <div className={styles.timer}>
-            <CountdownCircleTimer duration={props.duration} size={120} isPlaying onComplete={props.timeout} colors={['#2BAA6D', '#FF8C00', '#DC143C']} colorsTime={[10, 2, 0]}>
-                {({ remainingTime }) => remainingTime }
+            <CountdownCircleTimer duration={props.duration} size={120} isPlaying onComplete={props.timeout} colors={['#2BAA6D', '#FF8C00', '#DC143C']} colorsTime={[10, 5, 0]}>
+                {({ remainingTime }) => remainingTime}
             </CountdownCircleTimer>
         </div>
     )
