8e8a57ec3044348f570d44d5a5ff41590783f16d
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-03 19:42:34 -0300

    :hammer: Added a function for timeout

--- game/pages/index.tsx
+++ game/pages/index.tsx
@@ -15,13 +15,18 @@ export default function Home() {
   const [question, setQuestion] = useState(questionMock)
 
   function onResponse(indice: number) {
-    console.log(indice)
     setQuestion(question.replyWith(indice))
+  }
+
+  function timeout() {
+    if (question.unanswered) {
+      setQuestion(question.replyWith(-1))
+    }
   }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Question value={question} onResponse={onResponse} />
+      <Question value={question} onResponse={onResponse} timeout={timeout} />
     </div>
   )
 }
