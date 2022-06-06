ef527e6e2aa5a2db44fc62a8f1becde9143741c9
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-06 18:27:43 -0300

    :hammer: Adding some new functions and integrating useEffect

--- game/pages/index.tsx
+++ game/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import QuestionModel from '../model/question'
 import AnswerModel from '../model/answer'
@@ -11,8 +11,31 @@ const questionMock = new QuestionModel(1, 'Melhor cor?', [
   AnswerModel.correct('Preto')
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
+  const [questionIds, setQuestionIds] = useState<number[]>([])
   const [question, setQuestion] = useState(questionMock)
+
+  async function loadIdQuestion() {
+    const resp = await fetch(`${BASE_URL}/questionnaire`)
+    const questionIds = await resp.json()
+    setQuestionIds(questionIds)
+  }
+
+  async function loadQuestion(questionId: number) {
+    const resp = await fetch(`${BASE_URL}/questions/${questionId}`)
+    const json = await resp.json()
+    console.log(json)
+  }
+
+  useEffect(() => {
+    loadIdQuestion()
+  }, [])
+
+  useEffect(() => {
+    questionIds.length > 0 && loadQuestion(questionIds[0])
+  }, [questionIds])
 
   function questionAnswered(question: QuestionModel) {
 
@@ -23,9 +46,7 @@ export default function Home() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Questionnaire question={question} last={true} questionAnswered={questionAnswered} nextStep={nextStep} />
-    </div>
+    <Questionnaire question={question} last={true} questionAnswered={questionAnswered} nextStep={nextStep} />
   )
 }
 
