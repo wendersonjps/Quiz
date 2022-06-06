63bc8a97bd4585aa679c4bb3c181d608ab099b2f
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-06 20:49:17 -0300

    :hammer: Adjusting component

--- game/components/Timer.tsx
+++ game/components/Timer.tsx
@@ -3,6 +3,7 @@ import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import styles from '../styles/Timer.module.css'
 
 interface TimerProps {
+    key: any
     duration: number
     timeout: () => void
 }
