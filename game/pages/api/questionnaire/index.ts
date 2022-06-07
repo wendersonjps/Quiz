4e5061e5889f422a2d23e0bd15a02811c3983cfc
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-06 22:06:27 -0300

    :bug: Removing warning

--- game/pages/api/questionnaire/index.ts
+++ game/pages/api/questionnaire/index.ts
@@ -1,7 +1,7 @@
 import questions from '../questionBase'
 import { shuffle } from '../../../functions/arrays'
 
-export default (req, res) => {
+export default function questionnaire(req, res) {
     const ids = questions.map(question => question.id)
     res.status(200).json(shuffle(ids))
 }
